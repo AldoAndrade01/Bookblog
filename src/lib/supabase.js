789b1a7c2038b1a3c60425baed529618917c6220
Aldo Andrade <aldo.andrade123@gmail.com>
2025-02-27 02:57:46 -0600

    variables de entorno y nueva key jajaj

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Usa tus propias credenciales de Supabase
-const supabaseUrl = 'https://foqxfkkrtruohgeoemyt.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvcXhma2tydHJ1b2hnZW9lbXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNjI2MDksImV4cCI6MjA1NTczODYwOX0.qwUJftawZV3aIyWaX6UhKEstBDJPFaGPmbCqvy80Ajg';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
